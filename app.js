f22191c44615237c359986e2bacfed9d34882395
Maxwell Rutherford <javert.hugo@yandex.ru>
2022-07-04 13:22:03 +0300

    connect mongodb

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
+    } catch (error) {
+        console.error('Server error', error.message)
+        process.exit(1)
+    }
+}
+
+start()
